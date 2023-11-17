bc403e857b57a5246e297abb17481b4aa8611e7d
jessicacvetkovska <jessicacvetkovska@gmail.com>
2023-11-16 20:27:01 -0500

    Checkpoint 1 Achieved!

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -3,5 +3,6 @@ package edu.uc.cs3003.medava;
 public class SupplyChain {
     public static void main(String args[]) {
         // System.out.println("Hello, Jessica! This is the WOPR speaking.");
+        HospitalRunner.run();
     }
 }
