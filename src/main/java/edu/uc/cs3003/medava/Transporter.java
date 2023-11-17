f88f5420f1a73c0528f40854c6df91872117d579
jessicacvetkovska <jessicacvetkovska@gmail.com>
2023-11-16 20:21:59 -0500

    completed "Eastbound and Down" section

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,19 @@
 package edu.uc.cs3003.medava;
+import java.util.ArrayList;
+import java.util.List;
 public class Transporter {
+    public List<Medicine> goods;
+    private String mTransporterName;
+
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+    
+    {
+        goods = new ArrayList<Medicine>();
+    }
+
+    public void ship() {
+        // Do some shipping!
+    }
 }
